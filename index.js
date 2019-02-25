4a23e4304fefe6e25b767cb2c0dff522fa904b9d
Fabián Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2019-02-25 15:34:07 -0300

    Taller promesas

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 #!/usr/bin/env node
-
 const {fetchDog, fetchCat} = require('./lib/fetchAnimals');
 
 if(require.main === module){
-  console.log(fetchCat(300,300));
+  const daPromise = new Promise((resolve, reject)=>{
+    Promise.all([fetchDog(), fetchDog(), fetchDog()])
+      .then(daResults => {
+        return [daResults[2], daResults[1], daResults[0]];
+      })
+      .then(daResultsReverse => {
+        resolve(daResultsReverse);
+      })
+      .catch(reject);
+  });
+  //============= daPromise está en estado pendiente
+  daPromise
+    .then(daResults => {
+      console.log("Resultados de la promesa > "+daResults);
+    })
+    .catch(console.error);
+  
 }
 
 module.exports = {fetchCat, fetchDog};
